f17e7f1c42016dbd1dc32f252e3ef2d8b5217105
melon-sour <93722184+melon-vanilla-sour@users.noreply.github.com>
2024-09-08 19:34:55 +0900

    feat: add event listener to pin comments

--- script.js
+++ script.js
@@ -1,31 +1,31 @@
-console.log('pin comment loaded')
+console.log('pin-comment extension loaded')
 
 function addPinButtonToComments() {
+  // container for each comment element
   let commentsContainer = document.querySelectorAll('ytd-comment-thread-renderer')
 
   commentsContainer.forEach((commentContainer) => {
-    let comment = commentContainer.querySelector('#comment #body #main #header')
-    if (!comment.querySelector('.pin-button')) {
+    let comment = commentContainer.querySelector('#comment')
+    let commentHeader = commentContainer.querySelector('#body #main #header')
+    if (!commentHeader.querySelector('.pin-button')) {
       const pinButton = document.createElement('a')
       pinButton.textContent = 'pin comment'
       pinButton.className = 'pin-button'
-      comment.appendChild(pinButton)
-      console.log('button added')
+      pinButton.addEventListener('click', (event) => {
+        event.stopPropagation()
+        const rightColumn = document.querySelector('#secondary')
+        // parent node includes elements like replies i.e. the full comment
+        rightColumn.prepend(comment.parentNode)
+      })
+      commentHeader.appendChild(pinButton)
+      console.log('pin comment button added')
     }
   })
 }
 
-const waitForCommentsLoad = setInterval(() => {
-  console.log('waiting comments...')
-  const commentsContainer = document.querySelectorAll('ytd-comment-thread-renderer')
-  if (commentsContainer.length != 0) {
-    clearInterval(waitForCommentsLoad)
-    addPinButtonToComments()
-  }
-}, 1000)
-
 const waitForCommentsSectionLoad = setInterval(() => {
   console.log('waiting comments section...')
+  // the entire comments section that loads new comments with scroll
   const commentsSection = document.querySelector('#comments')
   if (commentsSection) {
     console.log(commentsSection)
@@ -33,7 +33,7 @@ const waitForCommentsSectionLoad = setInterval(() => {
     const observer = new MutationObserver((mutations) => {
       mutations.forEach((mutation) => {
         if (mutation.addedNodes.length > 0) {
-          console.log(mutation + 'node added!')
+          console.log(mutation + 'new comments loaded')
           addPinButtonToComments()
         }
       })
@@ -43,6 +43,5 @@ const waitForCommentsSectionLoad = setInterval(() => {
       childList: true,
       subtree: true,
     })
-    // addPinButtonToComments()
   }
 }, 1000)
