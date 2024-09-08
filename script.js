41b77e79d284175cfea39669d122c963b6166a2b
melon-sour <93722184+melon-vanilla-sour@users.noreply.github.com>
2024-09-09 03:58:37 +0900

    style: clean up code

--- script.js
+++ script.js
@@ -7,10 +7,12 @@ function addPinButtonToComments() {
   commentsContainer.forEach((commentContainer) => {
     let comment = commentContainer.querySelector('#comment')
     let commentHeader = commentContainer.querySelector('#body #main #header')
+
     if (!commentHeader.querySelector('.pin-button')) {
       const pinButton = document.createElement('a')
       pinButton.textContent = 'pin comment'
       pinButton.className = 'pin-button'
+
       pinButton.addEventListener('click', (event) => {
         event.stopPropagation()
         const rightColumn = document.querySelector('#secondary')
@@ -20,23 +22,21 @@ function addPinButtonToComments() {
         comment.parentNode.style.height = videoPlayerHeight + 'px'
         rightColumn.prepend(comment.parentNode)
       })
+
       commentHeader.appendChild(pinButton)
-      // console.log('pin comment button added')
     }
   })
 }
 
 const waitForCommentsSectionLoad = setInterval(() => {
-  console.log('waiting comments section...')
   // the entire comments section that loads new comments with scroll
   const commentsSection = document.querySelector('#comments')
+
   if (commentsSection) {
-    console.log(commentsSection)
     clearInterval(waitForCommentsSectionLoad)
     const observer = new MutationObserver((mutations) => {
       mutations.forEach((mutation) => {
         if (mutation.addedNodes.length > 0) {
-          // console.log(mutation + 'new comments loaded')
           addPinButtonToComments()
         }
       })
